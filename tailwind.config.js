2c49bd3e3e086783b4824fd27f7162b936d4f4ea
Aamir <aamir.1705@gmail.com>
2022-07-10 17:27:54 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    backgroundImage: {
+      'pack-train': "url('./assets/images/man-791049_1280.jpeg')",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
